dc214515ee179a85815dc20c3c3bbacd1a225749
Gonzalo Neira <gneira@bld.com.ar>
2022-10-28 16:00:33 -0300

    Ejercicios de repaso en casa

---
+++ Clase 21/ejercicio05.js
@@ -0,0 +1,20 @@
+//document.getElementById('mensaje')
+// const mensajeconst = document.getElementById('mensaje').value
+
+
+// mensaje.addEventListener('input', function handleChange(event) {
+//     console.log(event.target.value.length)
+// })
+
+// console.log(mensaje.value.length)
+// console.log(`${mensajeconst}`)
+
+
+
+
+
+function calcular(){
+    const mensajeconst = document.getElementById('mensaje').value
+    alert(`Su mensaje tiene ${mensajeconst.length} caracteres`)
+
+}
