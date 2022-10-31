2bc3a9286c6ebc00dfe55bbbbdd86742d135a613
Gonzalo Neira <gneira@bld.com.ar>
2022-10-31 12:09:51 -0300

    arrancamos con React

---
+++ clase22-react/src/App.js
@@ -0,0 +1,18 @@
+//import React from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+import Header from "./components/layout/Header";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Header/>
+      {/* el componente Header se puede escribir de cualquiera de las 2 maneras */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ clase22-react/src/components/layout/Header.js
@@ -0,0 +1,11 @@
+const Header = (props) =>{
+    return(
+        <div>
+        <p>Hola soy el header</p>
+        <p>klñsdfvñlsdkdñslksdñlvkmsdñlvmsdvmñldsvmsñldv
+        </p>
+        </div>
+    )
+}
+
+export default Header
